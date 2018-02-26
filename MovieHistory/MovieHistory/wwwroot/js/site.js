958e4bd34326fd27251a4e8139ca36c8ba9c1a5f
Courtney Seward <courtneyseward93@gmail.com>
2018-02-26 15:22:29 -0600

    more commits

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,7 +2,7 @@
     const userSearchString = $("#movieSearch").val()
     $.ajax({
         method: "GET",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=ec2f1e210a4d2150e069b9b93b4a4c12&language=en-US&query=${userSearchString}&page=1&include_adult=false`,
+        url: `https://api.themoviedb.org/3/search/movie?api_key=659e751fd435d62cb82ad337c2f6bd5elanguage=en-US&query=${userSearchString}&page=1&include_adult=false`,
         success: (Response => console.log(Response))
     })
 });
