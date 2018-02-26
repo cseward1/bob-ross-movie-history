af614a3071ee600675b047fb7cc735bdfb9a6436
Courtney Seward <courtneyseward93@gmail.com>
2018-02-26 11:03:11 -0600

    started a migrations and created a new database as the class followed along with Steve's Code

--- MovieHistory/MovieHistory/Data/Migrations/20180226162428_Initial.Designer.cs
+++ MovieHistory/MovieHistory/Data/Migrations/20180226162428_Initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace MovieHistory.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180226162428_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
